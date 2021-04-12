182091abde5a5f4b29ee84e334838d1b42eacd07
Sergey Eliseev <thefury014@gmail.com>
2021-04-12 23:17:49 +0900

    Small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
-// @ts-ignore
-import abc from './samples/sample1.ogg';
+import sample1 from './samples/sample1.ogg';
 import { AudioAnalyserFactory } from './utils/AudioContext';
 import { BasicVisualizer } from './visualizers/BasicVisualizer';
 
 const someAudio = new Audio(
-  // 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-  abc,
+  'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
+  // sample1,
 );
-
+someAudio.crossOrigin = 'anonymous';
 const WIDTH = 512;
 const HEIGHT = 500;
-const BUFFER_LENGTH = 1024;
+const FFT_SIZE = 1024;
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -41,10 +40,9 @@ function App() {
       const source = audioContext.createMediaElementSource(someAudio);
       source.connect(audioAnalyser);
       audioAnalyser.connect(audioContext.destination);
-      audioAnalyser.fftSize = BUFFER_LENGTH;
+      audioAnalyser.fftSize = FFT_SIZE;
       const bufferLength = audioAnalyser.frequencyBinCount;
       dataArrayRef.current = new Uint8Array(bufferLength);
-      audioAnalyser.getByteTimeDomainData(dataArrayRef.current);
       if (!canvasRef.current) {
         console.warn('where canvas');
         return;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare module '*.ogg' {
+  const content: string;
+  export default content;
+}
+declare module '*.mp3' {
+  const content: string;
+  export default content;
+}
